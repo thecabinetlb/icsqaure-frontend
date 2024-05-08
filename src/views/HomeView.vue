58178190b4bfccabbf28bd973bf1dd7f64942fe3
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-05-08 11:06:03 +0300

    added the homepage back

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import Hero from './components/Hero.vue';
-import Mission from './components/Mission.vue';
-import Services from './components/Services.vue';
-import Contact from './components/Contact.vue';
-import Subsription from './components/Subsription.vue';
-import ValueProposition from './components/ValueProposition.vue';
+import Hero from '../components/Hero.vue';
+import Mission from '../components/Mission.vue';
+import Services from '../components/Services.vue';
+import ContactForm from '../components/ContactForm.vue';
+import Subsription from '../components/Subsription.vue';
+import ValueProposition from '../components/ValueProposition.vue';
 
 </script>
 
@@ -15,6 +15,6 @@ import ValueProposition from './components/ValueProposition.vue';
     <ValueProposition/>
     <Services/>
     <Subsription/>
-    <Contact/>
+    <ContactForm/>
 </main>
 </template>
