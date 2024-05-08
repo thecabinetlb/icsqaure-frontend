754dcb8afd65bc2eaaca4f18a8e8c09fa97b35b8
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-05-08 10:59:13 +0300

    added the homevue

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import ContactView from '../views/ContactView.vue'
 import Contact from '../components/ContactForm.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', component: HomeView },
     { path: '/contact', component: ContactView },
     { path: '/#contact', component: Contact },
   ],
