2406a9464645fa472a560efac18259302529ec74
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-10-10 12:48:34 +0300

    placeholder

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -7,7 +7,7 @@ defineProps({
 
 </script>
 <template>
-    <a v-for="(item, key) in sociallinks" :key="key" :id="'go-to-' + item.name" :href="item.to" :target="item.target" class="h-[48px] rounded-full bg-accent1/30 flex items-center gap-2 hover:brightness-110 cursor-pointer">
+    <a v-for="(item, key) in data" :key="key" :id="'go-to-' + item.name" :href="item.to" :target="item.target" class="h-[48px] rounded-full bg-accent1/30 flex items-center gap-2 hover:brightness-110 cursor-pointer">
         <img :src="item.icon" :alt="item.name" width="48" height="48" center cover responsive loading="lazy"/>   
         <h2 v-show="isFooter" class="px-2 font-[500]">{{ item.content }}</h2>
     </a>
