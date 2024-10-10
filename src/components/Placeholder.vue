dc8bddd9984bb6bf28a4d98c609c58db654c7a14
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-10-10 14:57:44 +0300

    bug fix

--- src/components/Placeholder.vue
+++ src/components/Placeholder.vue
@@ -13,12 +13,15 @@ const sociallinks = [
     <img :src="logo" alt="IC2" width="45" height="54" class="mt-20 scale-150" loading="eager" />
 
   <div class="flex flex-col items-center justify-center w-11/12 h-full gap-10 mx-auto 2xl:gap-16 2xl:w-8/12 lg:w-10/12">
-    <h1 class="2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-[100] text-center text-accent1">
-      We're busy building something amazing.
-    </h1>
-    <h1 class="2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-[100] text-center text-accent1">
-      <span class="font-[900]">Stay tuned</span> for the future of innovation.
-    </h1>
+    <div class="space-y-3">
+      <h1 class="2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-[100] text-center text-accent1">
+        We're busy building something amazing.
+      </h1>
+      <h1 class="2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-[100] text-center text-accent1">
+        <span class="font-[900]">Stay tuned</span> for the future of innovation.
+      </h1>      
+    </div>
+
     <Stopwatch/>
     <div class="flex flex-wrap items-center justify-center gap-6 text-center">
       <p class="tracking-wide font-[400] lg:text-[24px] md:text-[20px] sm:text-[18px] text-accent2">
