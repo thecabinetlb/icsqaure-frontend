0c6d696def07610009773d4e1e7b85cb58b9b83d
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-10-10 14:53:59 +0300

    octover 21

--- src/components/Placeholder.vue
+++ src/components/Placeholder.vue
@@ -12,21 +12,13 @@ const sociallinks = [
 <section class="flex flex-col items-center justify-center h-screen mx-auto bg-center bg-cover bg-sky_v3">
     <img :src="logo" alt="IC2" width="45" height="54" class="mt-20 scale-150" loading="eager" />
 
-  <div class="flex flex-col items-center justify-center w-11/12 h-full gap-10 mx-auto 2xl:w-8/12 lg:w-10/12">
-    
-    <h1 class="2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-[500] text-center uppercase text-accent1">
-      We're busy building something amazing. Stay tuned for the future of innovation.
+  <div class="flex flex-col items-center justify-center w-11/12 h-full gap-10 mx-auto 2xl:gap-16 2xl:w-8/12 lg:w-10/12">
+    <h1 class="2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl font-[100] text-center text-accent1 lg:w-2/3">
+      We're busy building something amazing. <span class="font-[900]">Stay tuned</span> for the future of innovation.
     </h1>
     <Stopwatch/>
-    <div class="flex items-center justify-center w-full gap-3 px-2">
-      <span class="text-base font-[400] text-accent1">0%</span>
-      <div class="relative w-full border border-accent1 rounded-full h-2.5">
-        <div class="bg-accent1 h-2.5 rounded-full" style="width: 85%;"></div>
-      </div>
-      <span class="text-sm font-[400] text-accent1">100%</span>
-    </div>
-    <div class="flex items-center gap-6">
-      <p class="tracking-wide font-[400] lg:text-[24px] md:text-[20px] sm:text-[18px] text-center text-accent2">
+    <div class="flex flex-wrap items-center justify-center gap-6 text-center">
+      <p class="tracking-wide font-[400] lg:text-[24px] md:text-[20px] sm:text-[18px] text-accent2">
         Follow us for the latest updates:
       </p>
       <Socials :data="sociallinks"/>
--- src/components/Stopwatch.vue
+++ src/components/Stopwatch.vue
@@ -2,24 +2,24 @@
     <div class="space-y-10">
       <div v-if="timeLeft.total > 0" class="flex items-center justify-center gap-3 sm:gap-10">
         <div class="lg:min-w-[145px] sm:min-w-[81px] flex flex-col items-center justify-center gap-6">
-            <span class="rounded-[8px] sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.days }}</span>
+            <span class="rounded-[8px] lg:p-8 sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.days }}</span>
             <span class="font-[100] 2xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-md text-center text-accent2">Days</span>
         </div>
         <div class="lg:min-w-[145px] sm:min-w-[81px] flex flex-col items-center justify-center gap-6">
-            <span class="rounded-[8px] sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.hours }}</span>
-            <span class="font-[100] 2xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-md text-center text-accent2">Months</span>
+            <span class="rounded-[8px] lg:p-8 sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.hours }}</span>
+            <span class="font-[100] 2xl:text-3xl md:text-2xl sm:text-xl text-md text-center text-accent2">Months</span>
         </div>
         <div class="lg:min-w-[145px] sm:min-w-[81px] flex flex-col items-center justify-center gap-6">
-            <span class="rounded-[8px] sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.minutes }}</span>
-            <span class="font-[100] 2xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-md text-center text-accent2">Minutes</span>
+            <span class="rounded-[8px] lg:p-8 sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.minutes }}</span>
+            <span class="font-[100] 2xl:text-3xl md:text-2xl sm:text-xl text-md text-center text-accent2">Minutes</span>
         </div>
         <div class="lg:min-w-[145px] sm:min-w-[81px] flex flex-col items-center justify-center gap-6">
-            <span class="rounded-[8px] sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.seconds }}</span>
-            <span class="font-[100] 2xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-md text-center text-accent2">Seconds</span>
+            <span class="rounded-[8px] lg:p-8 sm:p-6 p-3 grid place-content-center font-[500] 2xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl tracking-widest text-accent1 shadow-lg bg-bg/30">{{ timeLeft.seconds }}</span>
+            <span class="font-[100] 2xl:text-3xl md:text-2xl sm:text-xl text-md text-center text-accent2">Seconds</span>
         </div>
     </div>
       <div v-else class="font-[500] 2xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg text-center text-accent1">
-        <span>Time's up!</span>
+        <span>The website will be live soon! Stay tuned!</span>
       </div>
     </div>
   </template>
@@ -27,7 +27,7 @@
   <script setup lang="ts">
   import { ref, onMounted } from "vue";
   
-  const targetDate = new Date("November 5, 2024 00:00:00").getTime();
+  const targetDate = new Date("October 21, 2024 00:00:00").getTime();
   const timeLeft = ref({ total: 0, days: "00", hours: "00", minutes: "00", seconds: "00" });
   
   const calculateTimeLeft = () => {
