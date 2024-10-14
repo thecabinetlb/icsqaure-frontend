2c112bd5eb2f9c23deb0211b491fd15bb058310c
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-10-14 15:09:19 +0300

    finished the contact form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import { RouterView } from 'vue-router';
+import Header from './components/layouts/Header.vue';
+import Footer from './components/layouts/Footer.vue';
 import Placeholder from './components/Placeholder.vue';
+
 
 </script>
 
 <template>
-    <main>
-      <Placeholder/>
-    </main>
+    <Placeholder/>
 </template>
--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,47 +1,79 @@
 <script setup>
 import axios from 'axios';
 import { ref, reactive, watch } from 'vue'
-
+// import {Checkbox, useRecaptchaProvider} from 'vue-recaptcha'
+// useRecaptchaProvider() 
+import { VueRecaptcha } from 'vue-recaptcha';
 defineProps({
     data: Array
 })
-useRecaptchaProvider()
-const ReCaptchaValid = ref(false)
+const sitekey = import.meta.env.VITE_RECAPTCHAV2_SITEKEY;
+const baseurl = import.meta.env.VITE_BASE_URL;
+
+const recaptcha = ref()
+const reCaptchaValid = ref(false)
+console.log('reCaptchaValid', reCaptchaValid.value)
 const hasErrorMessages = ref(false)
 const loading = ref(null)
 console.log('hasErrorMessages', hasErrorMessages)
 
+const onVerify = (response) => {
+  reCaptchaValid.value = true; // Set to true when verified
+};
+
+const onExpired = () => {
+  reCaptchaValid.value = false; // Reset if expired
+};
 const errors = {
+  TitleRequired: 'Title is required.',
   NameRequired: 'Name is required.',
   EmailRequired: 'Email is required.',
   EmailInvalid: 'Please specify a real email.',
-  CompanyNameRequired: 'Company Name is required.',
+  CountryRequired: 'Country Name is required.',
+  CityRequired: 'City Name is required.',
+  IndustryRequired: 'Industry is required.',
   PhoneRequired: 'Phone Number is required.',
   PhoneInvalid: 'Phone Number should be numeric.',
-  SubjectRequired: 'Subject is required.',
-  SubjectInvalid: 'Invalid subject. Please select either General Inquiries or Sales and Support.',
+  IndustryInvalid: 'Invalid industry. Please select either Smart Cities or Food and Beverage',
   MessageRequired: 'Please enter your message.'
 };
 
 const ErrorMessages = ref({
+    title: '',
     name: '',
     email: '',
     phone: '',
+    country: '',
+    city: '',
+    industry: '',
     company_name: '',
-    subject: '',
     message: ''
 })
 console.log('ErrorMessages',ErrorMessages)
 
 const formData = reactive({ 
     data: { 
+        Title : {
+            name: 'Title',
+            value : '',
+            isValid: null,
+            validationMessage: "",
+            required: true,            
+        },            
         Name : {
             name: 'Name',
             value : '',
             isValid: null,
             validationMessage: "",
             required: true,            
-        },    
+        },        
+        CompanyName : {
+            name: 'CompanyName',
+            value : '',
+            isValid: null,
+            validationMessage: "",
+            required: true,            
+        },
         Phone : {
             name: 'Phone',
             value : '',
@@ -56,19 +88,26 @@ const formData = reactive({
             validationMessage: "",
             required: true,            
         },            
-        CompanyName : {
-            name: 'CompanyName',
+        Country : {
+            name: 'Country',
             value : '',
             isValid: null,
             validationMessage: "",
             required: true,            
         },
-        Subject : {
-            name: 'Subject',
-            value : 'General Inquiries',
-            isValid: true,
+        City : {
+            name: 'City',
+            value : '',
+            isValid: null,
             validationMessage: "",
-            required: false,            
+            required: true,            
+        },
+        Industry : {
+            name: 'Industry',
+            value : '',
+            isValid: null,
+            validationMessage: "",
+            required: true,
         },
         Message : {
             name: 'Message',
@@ -132,40 +171,44 @@ const handleSubmit = () => {
     if (formData.isValid) {
         // Create a new data object with the data to be sent
         const data = {
+            title: formData.data.Title.value,
             name: formData.data.Name.value,
+            company_name: formData.data.CompanyName.value,
             email: formData.data.Email.value,
             phone: formData.data.Phone.value,
-            company_name: formData.data.CompanyName.value,
-            subject: formData.data.Subject.value,
+            country: formData.data.Country.value,
+            city: formData.data.City.value,
+            industry: formData.data.Industry.value,
             message: formData.data.Message.value
         };
         // Submit the data
-        axios.post('http://localhost:8000/api/v1/contact_submissions', data, {
+        axios.post(`${baseurl}/api/v1/contact_submissions`, data, {
             headers: {
                 'Access-Control-Allow-Origin': '*', // Replace * with the specific origin if needed
             }
         }).then(response => {
             console.log('Data submitted:', response.data);
-            // Clear form inputs
-            formData.data.value = {
-                Name: '',
-                Email: '',
-                Phone: '',
-                CompanyName: '',
-                Subject: '',
-                Message: ''
-            };
-        loading.value = false;
+             loading.value = false;
         }).catch(error => {
             console.error('Errors:', error.response.data.errors);
             hasErrorMessages.value = true;
             ErrorMessages.value = error.response.data.errors;
             loading.value = false;
         });
+        // Clear form inputs
+        formData.data.value = {
+            Title: '',
+            Name: '',
+            Email: '',
+            CompnayName: '',
+            Phone: '',
+            Country: '',
+            City: '',
+            Industry: '',
+            Message: ''
+        };
     } 
 };
-const sitekey = import.meta.env.VITE_RECAPTCHAV2_SITEKEY;
-
 </script>
 
 <template>
@@ -189,7 +232,7 @@ const sitekey = import.meta.env.VITE_RECAPTCHAV2_SITEKEY;
                 placeholder="Your Name" 
                 v-model="formData.data.Name.value"
                 :required="formData.data.Name.required" 
-                class="w-full p-3 border bg-accent1 boredr-accent2 rounded-[8px]"
+                class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px]"
                 :class="{'border-red-500 focus:border-red-500' : formData.data.Name.isValid===false}">
                 <p v-show="!formData.data.Name.isValid" className="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.Name.validationMessage}}</p>
             </div>
@@ -198,7 +241,7 @@ const sitekey = import.meta.env.VITE_RECAPTCHAV2_SITEKEY;
                 placeholder="example@gmail.com" 
                 v-model="formData.data.Email.value" 
                 :required="formData.data.Email.required" 
-                class="w-full p-3 border bg-accent1 boredr-accent2 rounded-[8px]"
+                class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px]"
                 :class="{'border-red-500 focus:border-red-500' : formData.data.Email.isValid===false}">
                 <p v-show="!formData.data.Email.isValid" className="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.Email.validationMessage}}</p>
             </div>      
@@ -207,7 +250,7 @@ const sitekey = import.meta.env.VITE_RECAPTCHAV2_SITEKEY;
                 placeholder="00961 71 654 231" 
                 v-model="formData.data.Phone.value"          
                 :required="formData.data.Phone.required" 
-                class="w-full p-3 border bg-accent1 boredr-accent2 rounded-[8px]"
+                class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px]"
                 :class="{'border-red-500 focus:border-red-500' : formData.data.Phone.isValid===false}">
                 <p v-show="!formData.data.Phone.isValid" className="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.Phone.validationMessage}}</p>
             </div> 
@@ -216,27 +259,58 @@ const sitekey = import.meta.env.VITE_RECAPTCHAV2_SITEKEY;
                 placeholder="Your Company's Name"
                 v-model="formData.data.CompanyName.value"              
                 :required="formData.data.CompanyName.required" 
-                class="w-full p-3 border bg-accent1 boredr-accent2 rounded-[8px]"                
+                class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px]"                
                 :class="{'border-red-500 focus:border-red-500' : formData.data.CompanyName.isValid===false}">
                 <p v-show="!formData.data.CompanyName.isValid" className="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.CompanyName.validationMessage}}</p>
             </div>    
-            <div class="w-full col-span-2 mb-3">
-                <select id="Subject" name="Subject"
-                v-model="formData.data.Subject.value"
-                :required="formData.data.Subject.required" 
-                class="w-full p-3 border bg-accent1 boredr-accent2 rounded-[8px] text-bg/50"                
-                :class="{'border-red-500 focus:border-red-500' : formData.data.Subject.isValid===false}">
-                    <option class="text-black" selected>General Inquiries</option>
-                    <option class="text-black">Sales and Support</option>
+            <div class="w-full col-span-2 sm:col-span-1">
+                <input type="text" id="Country" name="Country"  aria-label="Enter your Country"
+                placeholder="Your Country"
+                v-model="formData.data.Country.value"              
+                :required="formData.data.Country.required" 
+                class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px]"
+                :class="{'border-red-500 focus:border-red-500' : formData.data.Country.isValid===false}">
+                <p v-show="!formData.data.Country.isValid" class="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.Country.validationMessage}}</p>
+            </div>    
+            <div class="w-full col-span-2 sm:col-span-1">
+                <input type="text" id="City" name="City"  aria-label="Enter your City"
+                placeholder="Your City"
+                v-model="formData.data.City.value"              
+                :required="formData.data.City.required" 
+                class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px]"
+                :class="{'border-red-500 focus:border-red-500' :  formData.data.City.isValid===false}">
+                <p v-show="!formData.data.City.isValid" class="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.City.validationMessage}}</p>
+            </div>  
+            <div class="relative w-full col-span-2">
+                <select id="industry" name="Industry" aria-label="Select an industry"
+                    placeholder="Select an industry"
+                    v-model="formData.data.Industry.value"              
+                    :required="formData.data.Industry.required" 
+                    class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px] text-gray-400"
+                    :class="{'border-red-500 focus:border-red-500' : formData.data.Industry.isValid === false}">
+                    <option disabled value="" class="text-black">Select an industry</option>
+                    <option value="Smart Cities" class="text-black">Smart Cities</option>
+                    <option value="Food and Beverage" class="text-black">Food and Beverage</option>
+                    <option value="Manufacturing" class="text-black">Manufacturing</option>
+                    <!-- <option value="Oil and Gas" class="text-black">Oil and Gas</option> -->
+                    <option value="Energy" class="text-black">Energy</option>
+                    <option value="Water and Wasterwater" class="text-black">Water and Wasterwater</option>
+                    <option value="Metal, Mining and Minerals" class="text-black">Metal, Mining and Minerals</option>
                 </select>
-                <p v-show="!formData.data.Subject.isValid" className="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.Subject.validationMessage}}</p>
+                <!-- Arrow icon -->
+                <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                    <svg class="w-5 h-5 text-accent1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                    </svg>
+                </div>
+              <p v-show="!formData.data.Industry.isValid" class="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.Industry.validationMessage}}</p>
             </div>
             <div class="w-full col-span-2">
                 <textarea id="Message" name="Message" rows="5" 
                 placeholder="Briefly tell us about your project and your current goals. How can we help you?" 
                 v-model="formData.data.Message.value" 
                 :required="formData.data.Message.required" 
-                class="w-full p-3 border bg-accent1 boredr-accent2 rounded-[8px]"                
+                class="w-full p-3 border bg-accent1 border-accent2 rounded-[8px]"                
                 :class="{'border-red-500 focus:border-red-500' : formData.data.Message.isValid===false}"></textarea>
                 <p v-show="!formData.data.Message.isValid" className="ms-2 mb-2 font-[700] text-[12px] text-red-500">{{formData.data.Message.validationMessage}}</p>
             </div>
@@ -248,7 +322,7 @@ const sitekey = import.meta.env.VITE_RECAPTCHAV2_SITEKEY;
                     @verify="onVerify"
                     @expired="onExpired"
                     theme="dark"
-                    ></vue-recaptcha>
+                ></vue-recaptcha>
             </div>
             <!-- Submit -->
             <div class="flex flex-wrap items-center w-full gap-2">
